f2136240cbb768aaee6c3620bc6f367500c27cec
Unknown <bengordon1995@gmail.com>
2019-08-29 19:11:26 -0700

    First Build
    character controller, camera controller, two layer tilemap(collidable and non-collidable), basic sprites, etc...

---
+++ Capstone/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+	public float speed;
+ 
+ 	private Rigidbody2D rigid;
+
+ 	public float horizontal;
+ 	public float vertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	//inputs rounded to -1,0,1 values
+    	horizontal = Input.GetAxisRaw("Horizontal");
+    	vertical = Input.GetAxisRaw("Vertical");
+
+    	this.rigid.transform.position += new Vector3(horizontal * speed * Time.deltaTime, vertical * speed * Time.deltaTime);
+    }
+}
