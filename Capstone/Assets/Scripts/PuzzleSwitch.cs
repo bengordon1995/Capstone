af570893a2c3aefd0e52fa0aef41f1458062e1b5
Unknown <bengordon1995@gmail.com>
2019-09-23 15:11:53 -0700

    working version of the puzzle prefab

--- Capstone/Assets/Scripts/PuzzleSwitch.cs
+++ Capstone/Assets/Scripts/PuzzleSwitch.cs
@@ -12,11 +12,14 @@ public class PuzzleSwitch : MonoBehaviour
 	public Sprite onSprite;
 	public Sprite offSprite;
 	public SwitchPuzzle puzzle;
+    public PuzzleSwitch left;
+    public PuzzleSwitch right;
 
     // Start is called before the first frame update
     void Start()
     {
         this.spriteRenderer = GetComponent<SpriteRenderer>();
+        this.spriteRenderer.sprite = this.offSprite;
     }
 
     // Update is called once per frame
@@ -30,12 +33,22 @@ public class PuzzleSwitch : MonoBehaviour
     	this.isOn = !this.isOn;
     	if (this.isOn){this.spriteRenderer.sprite = onSprite;}
     	else {this.spriteRenderer.sprite = offSprite;}
+
+        this.left.isOn = !this.left.isOn;
+        if (this.left.isOn){this.left.spriteRenderer.sprite = onSprite;}
+        else {this.left.spriteRenderer.sprite = offSprite;}
+        
+        this.right.isOn = !this.right.isOn;
+        if (this.right.isOn){this.right.spriteRenderer.sprite = onSprite;}
+        else {this.right.spriteRenderer.sprite = offSprite;}
     }
 
     void OnCollisionEnter2D(Collision2D collision){
-    	if(collision.gameObject.tag == "projectile"){
-    		toggle();
-    		puzzle.checkSolved();
-    	} 
+        if (!transform.parent.gameObject.GetComponent<SwitchPuzzle>().isSolved){
+        	if(collision.gameObject.tag == "projectile"){
+        		toggle();
+        		transform.parent.gameObject.GetComponent<SwitchPuzzle>().checkSolved();
+        	} 
+        }
     }
 }
--- Capstone/Assets/Scripts/SwitchPuzzle.cs
+++ Capstone/Assets/Scripts/SwitchPuzzle.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections.Generic;
 
 /*Defines behavior for a switch puzzle:
 	The switch puzzle has 5 switches arranged in a circle, each can either be on or off
@@ -12,7 +13,7 @@ public class SwitchPuzzle : MonoBehaviour
 {
 
 	public bool isSolved;
-	public GameObject[] switches;
+	public List<PuzzleSwitch> switches = new List<PuzzleSwitch>();
 	public GameObject switchPrefab;
 	public float puzzleRadius;
 
@@ -20,15 +21,26 @@ public class SwitchPuzzle : MonoBehaviour
     void Start()
     {
     	this.isSolved = false;
-    	GameObject[] switches = new GameObject[5];
     	//instantiates 5 switches located around the center of the puzzle prefab
         for (int i = 0; i < 5; i++){
         	Vector3 tempSwitchLocation = this.gameObject.transform.position + Quaternion.Euler(0,0,72*i) * Vector3.up * puzzleRadius;
-        	GameObject tempSwitch = GameObject.Instantiate(switchPrefab, tempSwitchLocation, Quaternion.identity) as GameObject;
+        	GameObject tempSwitch = Instantiate(switchPrefab, tempSwitchLocation, Quaternion.identity);
         	tempSwitch.transform.SetParent(this.transform);
-        	switches[i] = tempSwitch;
-        	
+        	switches.Add(tempSwitch.GetComponent<PuzzleSwitch>());
         }
+
+        //assign relationships
+        //edge cases
+        this.switches[0].right = this.switches[1];
+        this.switches[0].left = this.switches[4];
+        this.switches[4].left = this.switches[3];
+        this.switches[4].right = this.switches[0];
+        //center cases
+        for (int i = 1; i < 4;i++){
+        	this.switches[i].left = this.switches[i-1];
+        	this.switches[i].right = this.switches[i+1];
+        }
+
     }
 
     // Update is called once per frame
@@ -39,8 +51,8 @@ public class SwitchPuzzle : MonoBehaviour
 
     //checks if all switches are on
     public bool checkSolved(){
-    	foreach (GameObject tempSwitchPrefab in this.switches){
-    		if (tempSwitchPrefab.GetComponent<PuzzleSwitch>().isOn == false){
+    	foreach (PuzzleSwitch tempSwitch in this.switches){
+    		if (tempSwitch.isOn == false){
     			return false;
     		}
     	}
