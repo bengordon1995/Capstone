c4b1fdb9f7961924370503ca7371931f6dc9bbbf
Colton Buchanan <coltonbuchanan@email.arizona.edu>
2019-11-12 10:54:12 -0700

    Added the add file for items

---
+++ Capstone/Assets/Scripts/Items/ScaleItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleItem : Item
+{
+    public float scaleX = 0.5f;
+    public float scaleY = 0.5f;
+    public float scaleZ = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    protected override void powerUpAction()
+    {
+        GameObject.Find("Player").transform.localScale += new Vector3 (scaleX, scaleY, scaleZ);
+
+    }
+
+
+}
---
+++ Capstone/Assets/Scripts/Items/TripleShot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TripleShot : Item
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    protected override void powerUpAction()
+    {
+        GameObject.Find("Player").GetComponent<ProjectileManager>().numOfProjectile *= 3;
+        GameObject.Find("Player").GetComponent<ProjectileManager>().reloadTime += 1f;
+    }
+}
