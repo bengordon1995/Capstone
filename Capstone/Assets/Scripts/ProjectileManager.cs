4bd820f26bac38dfef5bffc449ff9fbbe32d7137
Colton Buchanan <coltonbuchanan@email.arizona.edu>
2019-11-12 10:51:39 -0700

    Added Items and Item Spawner

--- Capstone/Assets/Scripts/ProjectileManager.cs
+++ Capstone/Assets/Scripts/ProjectileManager.cs
@@ -17,6 +17,11 @@ public class ProjectileManager : MonoBehaviour
     
 	private float timeSinceLastProjectile;
 
+    public int numOfProjectile = 1;
+    private int projectileAngle = 180;
+    private int flipper = -1;
+    private float newAngle;
+
 
     // Start is called before the first frame update
     void Start()
@@ -37,11 +42,26 @@ public class ProjectileManager : MonoBehaviour
                 }
                 horizontalShoot = shootVector.x;
                 verticalShoot = shootVector.y;
-        		GameObject newProj = Instantiate(projectilePrefab, GameState.Instance.player.transform.position + shootVector.normalized, Quaternion.identity);
-                //limit projectile flight time for performance
-                Destroy(newProj, projectileFlightTime);
-                newProj.GetComponent<Rigidbody2D>().velocity = shootVector.normalized * (projectileSpeed + GameState.Instance.player.GetComponent<PlayerController>().speed);
-        		timeSinceLastProjectile = 0;
+                newAngle = projectileAngle / numOfProjectile;
+                for (int i = 0; i < numOfProjectile; i++)
+                {
+                    GameObject newProj = Instantiate(projectilePrefab, GameState.Instance.player.transform.position + shootVector.normalized, Quaternion.identity);
+                    //limit projectile flight time for performance
+                    Destroy(newProj, projectileFlightTime);
+                    newProj.GetComponent<Rigidbody2D>().velocity = shootVector.normalized * (projectileSpeed + GameState.Instance.player.GetComponent<PlayerController>().speed);
+
+                    Debug.Log("Angle is ");
+                    Debug.Log(newAngle * flipper);
+                    if (numOfProjectile != 1)
+                    {   
+                        newProj.GetComponent<Rigidbody2D>().AddForce(new Vector3((newAngle * flipper) * verticalShoot, (newAngle * flipper) * horizontalShoot, 0f));
+                        Debug.Log(horizontalShoot);
+                        flipper *= -1;
+                        newAngle += projectileAngle / numOfProjectile;
+                    }
+
+                    timeSinceLastProjectile = 0;
+                }
         	}
         }
     }
