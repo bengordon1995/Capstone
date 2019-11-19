09507000d665157a307cfb9ee39ef190dc24e54a
Rafael Barreda <rafabarreda98@live.com>
2019-11-19 13:28:15 -0700

    Added sound manager script

---
+++ Capstone/Assets/Scripts/SoundMaganer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static AudioClip bulletSound;
+    static AudioSource audioSrc;
+
+    void Start ()
+    {
+        bulletSound = Sound.Load<AudioClip> ("270343__littlerobotsoundfactory__shoot-01");
+
+        audioSrc = GetComponent<AudioSource>();
+
+    }
+
+
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip) {
+            case "bulletSound":
+                audioSrc / PlayOneShot(bulletSound);
+                break;
+        }
+    }
+    }
+    
+
+    
+   
+
